4801108bafb482c42c604da4850d7e80bb509817
johannasmithy <167107638+johannasmithy@users.noreply.github.com>
2024-06-03 16:24:00 -0400

    commenting out trip and IUserRepo

--- NewProject2FantasticFour/Repos/IUserRepo.cs
+++ NewProject2FantasticFour/Repos/IUserRepo.cs
@@ -1,9 +1,9 @@
-interface IUserRepo
-{
-    public void AddUser(User u);
-    public User? GetUser(int id);
-    public List<User> GetAllUsers();
-    public void UpdateUser(User u);
-    public void DeleteUser(int id);
-    public void Save();
-}
+// interface IUserRepo
+// {
+//     public void AddUser(User u);
+//     public User? GetUser(int id);
+//     public List<User> GetAllUsers();
+//     public void UpdateUser(User u);
+//     public void DeleteUser(int id);
+//     public void Save();
+// }
--- NewProject2FantasticFour/obj/Debug/net8.0/NewProject2FantasticFour.AssemblyInfo.cs
+++ NewProject2FantasticFour/obj/Debug/net8.0/NewProject2FantasticFour.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewProject2FantasticFour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18ff183d4200f2f336f1c030f9cfbb98ce33bba2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d8b98b0a4ad58dd465158979f020f4c987580cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewProject2FantasticFour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewProject2FantasticFour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
